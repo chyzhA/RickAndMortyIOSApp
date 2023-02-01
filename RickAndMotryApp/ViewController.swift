90b191ed8ea50e9465667a979482a601cadb5c59
ChyzhA <a.o.chyzh@icloud.com>
2023-02-01 20:02:30 +0200

    Initial Commit

---
+++ RickAndMotryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMotryApp
+//
+//  Created by Artem Chyzh on 01.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
