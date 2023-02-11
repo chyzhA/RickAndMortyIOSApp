44a56ebf4a523b6b402c77714538f66ad4f26612
ChyzhA <a.o.chyzh@icloud.com>
2023-02-11 22:30:02 +0200

    Added Custom Cell and test data to UICollectionView

--- RickAndMotryApp/ViewControllers/ViewController.swift
+++ RickAndMotryApp/ViewControllers/ViewController.swift
@@ -9,11 +9,8 @@ import UIKit
 
 final class ViewController: UIViewController {
 
-    private let collectionOfCharacters: UICollectionView = {
-        let collection = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout.init())
-        return collection
-    }()
-    
+    private let collectionOfCharacters = UICollectionView(frame: .zero,
+                                                          collectionViewLayout: UICollectionViewFlowLayout())
     private let appLabel: UILabel = {
         let label = UILabel()
         label.text = "Rick and Morty"
@@ -25,6 +22,7 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collectionOfCharacters.frame = view.bounds
         view.backgroundColor = .systemBackground
         
         setupSubViews()
@@ -33,6 +31,20 @@ final class ViewController: UIViewController {
     }
 
     private func setupSubViews() {
+        
+        guard let layout = collectionOfCharacters.collectionViewLayout as? UICollectionViewFlowLayout else { return }
+        
+        let itemSize = collectionOfCharacters.bounds.width / 3 - 8
+        
+        layout.itemSize = .init(width: itemSize, height: itemSize + 30)
+        layout.minimumLineSpacing = 2
+        layout.minimumInteritemSpacing = 2
+        layout.scrollDirection = .vertical
+        
+        collectionOfCharacters.register(RMCollectionCell.self,
+                                        forCellWithReuseIdentifier: "cell")
+        collectionOfCharacters.delegate = self
+        collectionOfCharacters.dataSource = self
         
         view.addSubview(appLabel)
         view.addSubview(collectionOfCharacters)
@@ -77,3 +89,20 @@ final class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionOfCharacters.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        (cell as? RMCollectionCell)?.setImage(with: "person")
+        (cell as? RMCollectionCell)?.setName(withName: "Rick Sanchez")
+        
+        return cell
+        
+    }
+    
+    
+}
